5206f77cd74815cf29ec300a712be0294828aefe
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-11 23:46:19 +0300

    initing the abstract class

--- src/DatabaseFactory.java
+++ src/DatabaseFactory.java
@@ -1,2 +1,12 @@
-public class DatabaseFactory {
+public abstract class DatabaseFactory {
+
+
+    public static Database createDatabase(String type, String uri, String name) throws Exception {
+        return switch (type) {
+            case "MongoDB" -> new MongoDB(uri, name);
+            case "Postgresql" -> new Postgresql(uri, name);
+            case "MySql" -> new MySql(uri, name);
+            default -> throw new Exception("Not valid type");
+        };
+    }
 }
