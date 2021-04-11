5ec943634e1634cf7c54c6277ab682feac9fb028
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-11 23:35:56 +0300

    implementing postgres connection

--- src/Postgresql.java
+++ src/Postgresql.java
@@ -1,2 +1,13 @@
-public class Postgresql {
+public class Postgresql extends Database {
+
+    public Postgresql(String uri, String name) {
+        super(uri, name);
+    }
+
+    @Override
+    void connect() {
+        // different databases can have different ways of connections
+        System.out.println("Postgresql connection successfully done");
+        System.out.println("Database name { " + this.getName() + " }");
+    }
 }
