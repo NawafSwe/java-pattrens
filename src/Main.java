306d3114b436a0a1ef697d5767888a828992e0b9
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-12 00:13:30 +0300

    main tested with factory

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Objects;
+
 public class Main {
+
+    // to avoid typos
+    enum databasesEnum {MongoDB, Postgresql, MySql}
 
     public static void main(String[] args) {
         // write your code here
+        // declaring enum for databases
+
         // testing database factory
-        DatabaseFactory factory = new DatabaseFactory();
         Database[] databases = new Database[3];
 
         // mongoDB database
@@ -19,9 +25,27 @@ public class Main {
         String psqlUri = "localhost:psql";
 
         // testing factory class
+        try {
 
+            databases[0] = DatabaseFactory.createDatabase(databasesEnum.MongoDB.name(), mongoUri, mongoName);
+            // testing database for mongo if it was created or not
+            System.out.println(databases[0]);
 
+            // testing database for MySql if it was created or not
+            databases[1] = DatabaseFactory.createDatabase(databasesEnum.MySql.name(), mysqlUri, mysqlName);
+            System.out.println(databases[1]);
+
+            // testing database for Postgresql if it was created or not
+            databases[2] = DatabaseFactory.createDatabase(databasesEnum.Postgresql.name(), psqlUri, psqlName);
+            System.out.println(databases[2]);
+
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
 
 
     }
+
+
 }
+
