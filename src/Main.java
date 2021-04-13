b912c01babba83e15ff6a64e80a71f6dcbb41d96
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-13 17:08:43 +0300

    commenting code to test singleton

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,41 @@
 
 public class Main {
-
     // to avoid typos in strings
     enum databasesEnum {MongoDB, Postgresql, MySql}
 
     public static void main(String[] args) {
         // testing database factory
-        Database[] databases = new Database[3];
 
-        // mongoDB database
-        String mongoName = "MongoDB";
-        String mongoUri = "localhost:mongod";
-
-        // mysql database
-        String mysqlName = "MySql";
-        String mysqlUri = "localhost:msql";
-
-        // psql database
-        String psqlName = "Psql";
-        String psqlUri = "localhost:psql";
-
-        // testing factory class
-        try {
-
-            databases[0] = DatabaseFactory.createDatabase(databasesEnum.MongoDB.name(), mongoUri, mongoName);
-            // testing database for mongo if it was created or not
-            databases[0].connect();
-
-            // testing database for MySql if it was created or not
-            databases[1] = DatabaseFactory.createDatabase(databasesEnum.MySql.name(), mysqlUri, mysqlName);
-            databases[1].connect();
-
-            // testing database for Postgresql if it was created or not
-            databases[2] = DatabaseFactory.createDatabase(databasesEnum.Postgresql.name(), psqlUri, psqlName);
-            databases[2].connect();
-
-        } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
-        }
+//        // mongoDB database
+//        String mongoName = "MongoDB";
+//        String mongoUri = "localhost:mongod";
+//
+//        // mysql database
+//        String mysqlName = "MySql";
+//        String mysqlUri = "localhost:msql";
+//
+//        // psql database
+//        String psqlName = "Psql";
+//        String psqlUri = "localhost:psql";
+//
+//        // testing factory class
+//        // using try catch in case there is null
+//        try {
+//            Database mongo = DatabaseFactory.createDatabase(databasesEnum.MongoDB.name(), mongoUri, mongoName);
+//            // testing database for mongo if it was created or not
+//            mongo.connect();
+//
+//            // testing database for MySql if it was created or not
+//            Database mysql = DatabaseFactory.createDatabase(databasesEnum.MySql.name(), mysqlUri, mysqlName);
+//            mysql.connect();
+//
+//            // testing database for Postgresql if it was created or not
+//            Database psql = DatabaseFactory.createDatabase(databasesEnum.Postgresql.name(), psqlUri, psqlName);
+//            psql.connect();
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getLocalizedMessage());
+//        }
     }
 }
 
